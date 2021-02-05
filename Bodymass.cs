075851bc969d9110e18f25076d4a38f9c866b576
Jack Van Arsdall <jackvanarsdall@gmail.com>
2021-02-04 19:31:16 -0500

    BMI re-factoring

--- Bodymass.cs
+++ Bodymass.cs
@@ -4,34 +4,63 @@ namespace Palm_Pilot
 {
     public class Bodymass
     {
-        public void Bodymassindex()
+        
+        // declaring variables used below
+            public double Height { get; set; }
+            public double Weight { get; set; }
+            public double Bmi { get; private set; }
+
+        public Bodymass(double height, double weight)
         {
-            // declaring variables used below
-            int height;
-            int weight;
-            int bmi;
+            Height = height;
+            Weight = weight;
+            CalculateBMI();
+        }
+        
+        private void CalculateBMI()
+        {
+            this.Bmi = (this.Weight * 703) / (this.Height * this.Height);
+        }
 
-            //prompt user for weight in pounds and then height in inches
-            Console.WriteLine("Please enter your height (in inches): ");
-            height = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Please enter your weight (in pounds): ");
-            weight = Convert.ToInt32(Console.ReadLine());
-
-            bmi = (weight * 703)/(height * height);
-
-            if (bmi >= 18 && bmi <= 24)
+        public string DisplayBMI()
+        {
+            if (Bmi < 18)
             {
-                Console.WriteLine("Your BMI is {0}. You are normal weight.",bmi);
+                return "underweight";
             }
-            if (bmi < 18)
+            if (Bmi > 24)
             {
-                Console.WriteLine("Your BMI is {0}. You are underweight.",bmi);
+                return "fluffy";
             }
-            if (bmi > 24)
+            else
             {
-                Console.WriteLine("Your BMI is {0}. You are overweight.",bmi);
+                return "normal weight";
             }
         }
+        
+        // public void Bodymassindex()
+        // {          
+        //     //prompt user for weight in pounds and then height in inches
+        //     Console.WriteLine("Please enter your height (in inches): ");
+        //     height = Convert.ToInt32(Console.ReadLine());
+
+        //     Console.WriteLine("Please enter your weight (in pounds): ");
+        //     weight = Convert.ToInt32(Console.ReadLine());
+
+        //     bmi = (weight * 703)/(height * height);
+
+        //     if (bmi >= 18 && bmi <= 24)
+        //     {
+        //         Console.WriteLine("Your BMI is {0}. You are normal weight.",bmi);
+        //     }
+        //     if (bmi < 18)
+        //     {
+        //         Console.WriteLine("Your BMI is {0}. You are underweight.",bmi);
+        //     }
+        //     if (bmi > 24)
+        //     {
+        //         Console.WriteLine("Your BMI is {0}. You are overweight.",bmi);
+        //     }
+        // }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,8 +8,6 @@ namespace Palm_Pilot
         {
             string response;
             
-            Console.WriteLine("Hello World!");
-            
             Console.WriteLine("Please enter or select 1 program: ");
             response = Console.ReadLine();
 
@@ -19,14 +17,27 @@ namespace Palm_Pilot
             }
             if (response == "1")
             {
-                var instance = new Bodymass();
-                instance.Bodymassindex();
+                LoadBodyMass();
             }
             else 
             {
                 Console.WriteLine("Goodbye!");
-            }
+            }          
+        }
+
+        static void LoadBodyMass()
+        {
+            //prompt user for weight in pounds and then height in inches
+            Console.WriteLine("Please enter your height (in inches): ");
+            var height = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter your weight (in pounds): ");
+            var weight = Convert.ToDouble(Console.ReadLine());
             
+            Bodymass instance = new Bodymass(height, weight);
+
+            // Console.WriteLine("Your BMI is: {0}. You are {1}.", instance.Bmi.ToString("d2"), instance.DisplayBMI());
+            Console.WriteLine("Your BMI is: {0:f2}. You are {1}.", instance.Bmi, instance.DisplayBMI());
         }
     }
 }
